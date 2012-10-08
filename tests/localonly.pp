c7c477ef84e3deb46d548cb1065dc56bbc323e3b
Chris Barker <cbarker@puppetlabs.com>
2012-10-07 17:22:00 -0700

    Initial commit File creates basic PFS when running in puppet apply mode on centos

---
+++ tests/localonly.pp
@@ -0,0 +1,33 @@
+#local only test of package file service
+package {'openssh':
+  name   => 'openssh'
+  ensure => present,
+}
+
+file {'sshd_config':
+  name    => '/etc/ssh/sshd_config',
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  notify  => Service['opensshd'],
+  content => "Protocol 2
+SyslogFacility AUTHPRIV
+PermitRootLogin yes 
+PasswordAuthentication yes 
+GSSAPIAuthentication no
+GSSAPICleanupCredentials yes
+UsePAM yes
+ChallengeResponseAuthentication no
+X11Forwarding yes
+Subsystem sftp  /usr/libexec/openssh/sftp-server",
+  
+}
+
+service {'opensshd':
+  name    => 'opensshd',
+  require => Package['openssh'],
+}
+
+
+
