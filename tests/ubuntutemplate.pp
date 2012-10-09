1baa4cfeca77c91d62872c9b69bc240ac9899dfe
Chris Barker <cbarker@puppetlabs.com>
2012-10-08 19:00:52 -0700

    creates ubuntutemplate, sshd_config.erb

---
+++ tests/ubuntutemplate.pp
@@ -0,0 +1,23 @@
+#local only test of package file service
+package {'openssh-server':
+  name   => 'openssh-server',
+  ensure => present,
+}
+
+file {'sshd_config':
+  name    => '/etc/ssh/sshd_config',
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0600',
+  content => template("opensshpfs/sshd_config.erb"),
+  notify  => Service['ssh'],
+}
+
+service {'ssh':
+  name    => 'ssh',
+  require => Package['openssh-server'],
+}
+
+
+
