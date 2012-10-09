370b30d5979297cc4110686854257e4012f6f2a1
Chris Barker <cbarker@puppetlabs.com>
2012-10-08 19:34:06 -0700

    adds generic tests

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,11 +2,11 @@ class opensshpfs::params {
   case $operatingsystem {
     centos: {
       $sshd_packagename = "openssh",
-      $sshd_servicename = "opensshd"
+      $sshd_servicename = "opensshd",
     }
     ubuntu: {
       $sshd_packagename = "openssh-server",
-      $sshd_servicename = "ssh"
+      $sshd_servicename = "ssh",
     }
     default: {
       fail("Unrecognized operating system for opensshpfs")
