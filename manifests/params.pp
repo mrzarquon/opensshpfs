c99e06c843b2282a3038345f54045f16e0713f0b
Chris Barker <cbarker@puppetlabs.com>
2012-10-08 19:53:18 -0700

    fixed params typos

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,12 +1,12 @@
 class opensshpfs::params {
-  case $operatingsystem {
-    centos: {
-      $sshd_packagename = "openssh",
-      $sshd_servicename = "opensshd",
+  case $osfamily {
+    redhat: { 
+      $sshd_packagename = "openssh"
+      $sshd_servicename = "opensshd"
     }
-    ubuntu: {
-      $sshd_packagename = "openssh-server",
-      $sshd_servicename = "ssh",
+    debian: { 
+      $sshd_packagename = "openssh-server" 
+      $sshd_servicename = "ssh"
     }
     default: {
       fail("Unrecognized operating system for opensshpfs")
