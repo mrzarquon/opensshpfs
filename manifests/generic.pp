194ab278f5282e86b2e6a2b23ee3cd6bd09c2e6e
Chris Barker <cbarker@puppetlabs.com>
2012-10-08 19:31:57 -0700

    Adds params and generic class
    Works on ubuntu and centos now

---
+++ manifests/generic.pp
@@ -0,0 +1,28 @@
+#local only test of package file service
+class opensshpfs::generic(
+  $sshd_packagename = $opensshpfs::params::sshd_packagename,
+  $sshd_servicename = $opensshpfs::params::sshd_servicename
+) inherits opensshpfs::params {
+
+  $PermitRootLogin = yes
+  $PasswordAuthentication = yes
+
+  package { $sshd_packagename: 
+    ensure => present,
+  }
+
+  file {'sshd_config':
+    name    => '/etc/ssh/sshd_config',
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    content => template("opensshpfs/sshd_config.erb"),
+    notify  => Service[$sshd_servicename],
+  }
+
+  service {$sshd_servicename:
+    require => Package[$sshd_packagename],
+  }
+}
+
---
+++ params.pp
@@ -0,0 +1,15 @@
+class opensshpfs::params {
+  case $operatingsystem {
+    centos: {
+      $sshd_packagename = "openssh",
+      $sshd_servicename = "opensshd"
+    }
+    ubuntu: {
+      $sshd_packagename = "openssh-server",
+      $sshd_servicename = "ssh"
+    }
+    default: {
+      fail("Unrecognized operating system for opensshpfs")
+    }
+  }
+}
